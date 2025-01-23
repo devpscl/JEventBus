70b7cf3deb022d5bc34dfc1b036e20b07c3c0342
Pascal <69166882+devpscl@users.noreply.github.com>
2025-01-23 21:30:25 +0100

    Add SubscribeEvent annotation

---
+++ src/main/java/net/devpscl/eventbus/annotation/SubscribeEvent.java
@@ -0,0 +1,18 @@
+package net.devpscl.eventbus.annotation;
+
+import net.devpscl.eventbus.util.EventPriority;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface SubscribeEvent {
+
+  @NotNull EventPriority priority() default EventPriority.DEFAULT;
+  @NotNull String tag() default "";
+
+}
